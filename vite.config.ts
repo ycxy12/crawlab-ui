f268d6ab259e18eac11121ce2e71596d2a4191dc
caoyong <>
2025-02-22 22:35:08 +0800

    修改ip

--- vite.config.ts
+++ vite.config.ts
@@ -5,56 +5,12 @@ import dynamicImport from 'vite-plugin-dynamic-import';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
+  base: '/',  // 如果是子路径部署，修改为 '/my-app/'
   build: {
-    lib: {
-      name: 'crawlab-ui',
-      entry: resolve(__dirname, 'src/index.ts'),
-      fileName: 'crawlab-ui',
-    },
+    outDir: 'dist',  // 设置输出目录
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        'vuex',
-        'axios',
-        'element-plus',
-        '@element/icons',
-        '@fortawesome/fontawesome-svg-core',
-        '@fortawesome/free-brands-svg-icons',
-        '@fortawesome/free-regular-svg-icons',
-        '@fortawesome/free-solid-svg-icons',
-        '@fortawesome/vue-fontawesome',
-        'echarts',
-        'atom-material-icons',
-        'simplemde',
-        'monaco-editor',
-      ],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-          'vue-router': 'VueRouter',
-          'vue-i18n': 'VueI18n',
-          vuex: 'Vuex',
-          axios: 'axios',
-          'element-plus': 'ElementPlus',
-          '@element/icons': 'ElementIcons',
-          '@fortawesome/fontawesome-svg-core': 'FontAwesomeSvgCore',
-          '@fortawesome/free-brands-svg-icons': 'FontAwesomeBrandsSvgIcons',
-          '@fortawesome/free-regular-svg-icons': 'FontAwesomeRegularSvgIcons',
-          '@fortawesome/free-solid-svg-icons': 'FontAwesomeSolidSvgIcons',
-          '@fortawesome/vue-fontawesome': 'FontAwesomeVue',
-          echarts: 'Echarts',
-          'atom-material-icons': 'AtomMaterialIcons',
-          'simplemde': 'SimpleMDE',
-          'monaco-editor': 'monaco',
-        }
-      }
-    },
+      input: resolve(__dirname, 'index.html'),  // 入口文件
+    }
   },
   resolve: {
     dedupe: ['vue'],
